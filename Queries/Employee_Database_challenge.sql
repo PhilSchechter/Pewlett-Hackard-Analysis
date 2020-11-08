939e007aecb57bb9947cdb7654595602b9a2855b
unknown <phil.schechter@gmail.com>
2020-11-08 18:36:20 -0500

    section 2 of the challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -11,3 +11,39 @@ INNER JOIN titles as t
 	on (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) 
+	emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- upcoming retirements by job title
+SELECT COUNT(*), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(*) DESC;
+
+-- preliminary version of the challenge, section 2
+SELECT
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	d.from_date,
+	d.to_date
+--	title.title
+FROM employees as e
+INNER JOIN dept_emp as d
+	on (e.emp_no = d.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (d.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+
+
